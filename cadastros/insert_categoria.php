3210ceba14a2e3e240097e7a0e61569bcb2bb912
Bruno Rodrigues <bruno.ptr02@gmail.com>
2024-01-08 22:36:39 -0300

    feat: Get Form Categoria

---
+++ cadastros/insert_categoria.php
@@ -0,0 +1,18 @@
+<?php
+include("../menu.php");
+$categoria = $_GET['nome'];
+echo $categoria;
+?>
+
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <title></title>
+</head>
+
+<body>
+
+</body>
+
+</html>
