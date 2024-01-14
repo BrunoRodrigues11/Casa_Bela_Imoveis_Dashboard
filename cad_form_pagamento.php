244468b6b49d0e8213598620b0240cc81d7db41e
Bruno Rodrigues <bruno.ptr02@gmail.com>
2024-01-14 14:03:45 -0300

    feat: Payment type registration

--- cad_form_pagamento.php
+++ cad_form_pagamento.php
@@ -15,7 +15,7 @@
                     <div class="card-header border-1">
                         <h3 class="mb-0">Cadastro de Pagamento</h3>
                     </div>
-                    <form action="">
+                    <form action="cadastros/insert_pagamento.php" method="get">
                         <div class="container">
                             <div class="row">
                                 <div class="col-md-6">
@@ -30,7 +30,7 @@
                         </div>
                         <div class="card-footer py-4">
                             <div style="text-align: right">
-                                <button href="#" class="btn btn-icon btn-success" type="submit">
+                                <button class="btn btn-icon btn-success" type="submit">
                                     <span class="btn-inner--icon"><i class="ni ni-check-bold"></i></span>
                                     <span class="btn-inner--text">Cadastrar</span>
                                 </button>
